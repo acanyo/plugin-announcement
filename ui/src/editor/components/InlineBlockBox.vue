34fec0a593808d8afbddc081bff61c099c4a31cb
acanyo <30819792@qq.com>
2025-08-14 17:18:50 +0800

    增加删除API

---
+++ ui/src/editor/components/InlineBlockBox.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import { NodeViewWrapper } from "@halo-dev/richtext-editor";
+</script>
+<template>
+  <node-view-wrapper as="div" class=":uno: inline-block-box inline-block">
+    <slot></slot>
+  </node-view-wrapper>
+</template>
