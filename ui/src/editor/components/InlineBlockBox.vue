803253ebedb38d62486e534c02097323c879aaab
acanyo <30819792@qq.com>
2025-08-15 11:06:36 +0800

    切换为富文本编辑器

--- ui/src/editor/components/InlineBlockBox.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-import { NodeViewWrapper } from "@halo-dev/richtext-editor";
-</script>
-<template>
-  <node-view-wrapper as="div" class=":uno: inline-block-box inline-block">
-    <slot></slot>
-  </node-view-wrapper>
-</template>
